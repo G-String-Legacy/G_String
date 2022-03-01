db31b555c50a85917634d9435f8470469435216d
Ralph Bloch <ralph.bloch@bell.net>
2022-03-01 16:09:14 -0500

    Add files via upload

---
+++ src_2022_01_16/resources/help/Help_0.tf
@@ -0,0 +1,13 @@
+<title>Starting an analysis</title>
+<para>This is the start up screen. You find your choices in the menu bar at the top of this screen. Here are some of the things, you can do:</para>
+<sub>File > Close|Never forget, you can exit the program any time you wish.</sub>
+<sub>File > Start Over|If you happen'd to have made a false entry, you can always restart.</sub>
+<sub>Action > Start New|If you have to start analyzing a new problem from scratch, this is where you start. The program will then guide you step by step through the whole process. It leads you to the desired G - Study results and the chance to do a D - Study.</sub>
+<sub>Action > Use Existing|At the end, it leads to the same outcome. But instead of having to enter the parameters for each step by hand, G_String gathers them from a previously established file.</sub>
+<sub>Action > Auto Index|This choice enables G_String_V to extract the actual sample sizes directly from the data file, rather than requiring you to enter them manually.</sub>
+<sub>Setup > Log Level|allows you to set the "Log Level". If the program misbehaves, you can crank up the Log Level to 'FINEST', and repeat the steps that led to disaster. G_String will write out a log file that helps the developer to find the cause of the problem.</sub>
+<sub>Setup > Preferences|allows expert users to modify some functionality and appearance of G_String_V.</sub>
+<sub>Setup > Set Home Directory|Before you can use G_STRING_V the first time, you have to establish a directory, the program can use exclusively for its calculations.
+<sub>Next Step|When you think, you have completed the task for this step, click on this button. It will take you to the next step.</sub>
+<sub>Previous Step|Allows you to return to previous steps.</sub>
+<sub>Synthesize Data|This new feature in G_String_VI allows experts to generate synthetic data files.

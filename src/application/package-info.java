10517285c2113717279cd8148f90daab26c279bf
Papa-26 <ralph.bloch@bell.net>
2022-06-17 06:05:29 -0400

    ongoing updates and maintenance

--- src/application/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * Package 'application' contains the entry point <code>Main</code> for GS_L
- * 
- * and a non-commented <code>application.css</code>
- * 
- * @author Ralph Bloch
- */
-package application;
--- src/resources/help/package-info.java
+++
@@ -1,6 +0,0 @@
-/**
- * <code>package resources.help</code> contains 21 text files (rudimentary mark-up) for contextual other help and other screens.
- * 
- * @author Ralph Bloch
- */
-package resources.help;
